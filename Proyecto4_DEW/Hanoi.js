778479d43ad22483bf5672df89d78a8c2d2947cb
Patricio <patriciosoriabenitez@gmail.com>
2024-10-21 09:55:45 +0100

    imagen hanoi y matriz

---
+++ Proyecto4_DEW/Hanoi.js
@@ -0,0 +1,27 @@
+var hanoi = [[]];
+
+for (var i = 0; i < 3; i++){
+    hanoi[i] = 1;
+    for(var j = 0; j < 3; j++){
+        hanoi[j] = 1;
+    }
+}
+
+
+
+
+
+class torres{
+
+}
+
+function iniciar(){
+}
+
+
+function guardar(){}
+
+
+function cargar(){}
+
+function mover(){}

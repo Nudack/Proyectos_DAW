21e996981b443059b8263a2e9b0a00c98e3ca3ca
Patricio <patriciosoriabenitez@gmail.com>
2024-10-29 13:57:11 +0000

    actualizado CRUD
    he actualizado el CRUD a lo que esta actualmente el CRUD de juanra falta cambiarlo a la tabla de usuarios

--- PHP/Biblioteca-Patricio/plantilla/plantilla.php
+++ PHP/Biblioteca-Patricio/plantilla/plantilla.php
@@ -39,10 +39,172 @@ class Plantilla{
     }
 
     static function menu(){
+        return "<!-- Navigation -->
+    <nav class=\"navbar navbar-expand-lg navbar-dark navbar-custom fixed-top\">
 
+        <!-- Text Logo - Use this if you don't have a graphic logo -->
+        <!-- <a class=\"navbar-brand logo-text page-scroll\" href=\"index.html\">Corso</a> -->
+
+        <!-- Image Logo -->
+        <a class=\"navbar-brand logo-image\" href=\"index.html\"><img src=\"images/logo.svg\" alt=\"alternative\"></a> 
+        
+        <!-- Mobile Menu Toggle Button -->
+        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarsExampleDefault\" aria-controls=\"navbarsExampleDefault\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+            <span class=\"navbar-toggler-awesome fas fa-bars\"></span>
+            <span class=\"navbar-toggler-awesome fas fa-times\"></span>
+        </button>
+        <!-- end of mobile menu toggle button -->
+
+        <div class=\"collapse navbar-collapse\" id=\"navbarsExampleDefault\">
+            <ul class=\"navbar-nav ml-auto\">
+                <li class=\"nav-item\">
+                    <a class=\"nav-link page-scroll\" href=\"#register\">REGISTER <span class=\"sr-only\">(current)</span></a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link page-scroll\" href=\"#description\">DETAILS</a>
+                </li>
+
+                <!-- Dropdown Menu -->          
+                <li class=\"nav-item dropdown\">
+                    <a class=\"nav-link dropdown-toggle page-scroll\" href=\"#date\" id=\"navbarDropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">DATE</a>
+                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                        <a class=\"dropdown-item\" href=\"article-details.html\"><span class=\"item-text\">ARTICLE DETAILS</span></a>
+                        <div class=\"dropdown-divider\"></div>
+                        <a class=\"dropdown-item\" href=\"terms-conditions.html\"><span class=\"item-text\">TERMS CONDITIONS</span></a>
+                        <div class=\"dropdown-divider\"></div>
+                        <a class=\"dropdown-item\" href=\"privacy-policy.html\"><span class=\"item-text\">PRIVACY POLICY</span></a>
+                    </div>
+                </li>
+                <!-- end of dropdown menu -->
+
+                <li class=\"nav-item\">
+                    <a class=\"nav-link page-scroll\" href=\"#contact\">CONTACT</a>
+                </li>
+            </ul>
+            <span class=\"nav-item social-icons\">
+                <span class=\"fa-stack\">
+                    <a href=\"#your-link\">
+                        <i class=\"fas fa-circle fa-stack-2x\"></i>
+                        <i class=\"fab fa-facebook-f fa-stack-1x\"></i>
+                    </a>
+                </span>
+                <span class=\"fa-stack\">
+                    <a href=\"#your-link\">
+                        <i class=\"fas fa-circle fa-stack-2x\"></i>
+                        <i class=\"fab fa-twitter fa-stack-1x\"></i>
+                    </a>
+                </span>
+            </span>
+        </div>
+    </nav> <!-- end of navbar -->
+    <!-- end of navigation -->
+    ";
     }
 
     static function footer(){
+        return "
+            <!-- Footer -->
+            <div class=\"footer\">
+                <div class=\"container\">
+                    <div class=\"row\">
+                        <div class=\"col-md-3\">
+                            <div class=\"footer-col first\">
+                                <h5>About Corso</h5>
+                                <p class=\"p-small\">We're passionate about teaching people how to do better SEO for their online presence</p>
+                            </div>
+                        </div> <!-- end of col -->
+                        <div class=\"col-md-3\">
+                            <div class=\"footer-col second\">
+                                <h5>Links</h5>
+                                <ul class=\"list-unstyled li-space-lg p-small\">
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"terms-conditions.html\">Terms & Conditions</a></div>
+                                    </li>
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"privacy-policy.html\">Privacy Policy</a></div>
+                                    </li>
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"article-details.html\">Article Details</a></div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div> <!-- end of col -->
+                        <div class=\"col-md-3\">
+                            <div class=\"footer-col third\">
+                                <h5>Links</h5>
+                                <ul class=\"list-unstyled li-space-lg p-small\">
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"article-details.html\">Article Details</a></div>
+                                    </li>
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"terms-conditions.html\">Terms & Conditions</a></div>
+                                    </li>
+                                    <li class=\"media\">
+                                        <i class=\"fas fa-square\"></i>
+                                        <div class=\"media-body\"><a href=\"privacy-policy.html\">Privacy Policy</a></div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div> <!-- end of col -->
+                        <div class=\"col-md-3\">
+                            <div class=\"footer-col fourth\">
+                                <h5>Social Media</h5>
+                                <p class=\"p-small\">For news & updates follow us</p>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-facebook-f\"></i>
+                                </a>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-twitter\"></i>
+                                </a>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-pinterest-p\"></i>
+                                </a>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-instagram\"></i>
+                                </a>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-linkedin-in\"></i>
+                                </a>
+                                <a href=\"#your-link\">
+                                    <i class=\"fab fa-youtube\"></i>
+                                </a>
+                            </div> 
+                        </div> <!-- end of col -->
+                    </div> <!-- end of row -->
+                </div> <!-- end of container -->
+            </div> <!-- end of footer -->  
+            <!-- end of footer -->
 
+
+            <!-- Copyright -->
+            <div class=\"copyright\">
+                <div class=\"container\">
+                    <div class=\"row\">
+                        <div class=\"col-lg-12\">
+                            <p class=\"p-small\">Copyright © 2020 <a href=\"https://inovatik.com\">Inovatik</a> - All rights reserved</p>
+                            <p class=\"p-small\">Distributed By: <a href=\"https://themewagon.com\" target=\"_blank\">ThemeWagon</a></p>
+                        </div> <!-- end of col -->
+                    </div> <!-- enf of row -->
+                </div> <!-- end of container -->
+            </div> <!-- end of copyright --> 
+            <!-- end of copyright -->
+            
+                
+            <!-- Scripts -->
+            <script src=\"js/jquery.min.js\"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+            <script src=\"js/popper.min.js\"></script> <!-- Popper tooltip library for Bootstrap -->
+            <script src=\"js/bootstrap.min.js\"></script> <!-- Bootstrap framework -->
+            <script src=\"js/jquery.easing.min.js\"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+            <script src=\"js/jquery.countdown.min.js\"></script> <!-- The Final Countdown plugin for jQuery -->
+            <script src=\"js/swiper.min.js\"></script> <!-- Swiper for image and text sliders -->
+            <script src=\"js/jquery.magnific-popup.js\"></script> <!-- Magnific Popup for lightboxes -->
+            <script src=\"js/validator.min.js\"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+            <script src=\"js/scripts.js\"></script> <!-- Custom scripts -->
+        ";
     }
 }
---
+++ PHP/template/literal/literal.php
@@ -0,0 +1,41 @@
+<?php
+
+    class Literal
+    {
+        //instancia única
+        private static $instance;
+
+
+        public function __construct($idioma='ES')
+        {
+
+            switch($idioma)
+            {
+                case 'ES':
+                    $this->lit = [
+                        'name'        => 'Nombre'
+                       ,'description' => 'Descripción'
+                       ,'autor'       => 'Autor'
+                       ,'editorial'   => 'Editorial'
+                       ,'enviar'      => 'Enviar'
+                       ,'error_gen'   => 'El campo es inválido'
+
+                    ];
+
+                break;
+            }
+
+        }
+
+
+        static public function getInstance()
+        {
+
+            if (empty(self::$instance))
+            {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+    }
